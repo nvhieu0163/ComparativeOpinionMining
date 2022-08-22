5eb4559b416f037796e4181b187e556dd924e199
[nvhieu0163] <[nguyenvuhieu2015@gmail.com]>
2022-08-22 11:55:32 +0700

    add preprocessing file

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,6 +1,7 @@
 from modules.feature_selection import feature_select_chi2
 from config import *
-
+from modules.load_data.load_task_data import load_IBO_task_data
+from modules.preprocessing.tag_IBO import tag_IBO_POS_for_each_word, tag_IBO_for_stc
 
 if __name__ == '__main__':
     
@@ -13,6 +14,22 @@ if __name__ == '__main__':
 
 
     #Get top words feature using Chi2 for SP task
-    feature_select_chi2.run_feature_selection_chi2(INPUT_LIST, SP_LABEL_LIST, SP_TEST_PATH, 'SP')
+    feature_select_chi2.run_feature_selection_chi2(INPUT_LIST, SP_LABEL_LIST, SP_TRAIN_PATH, 'SP')
 
 
+    #IBO and POS tagging for NER task
+    IBO_data = load_IBO_task_data('./dataset/input/test_IBO.csv',
+                    stc_idx_col_name='sentence_idx',
+                    stc_col_name='stc',
+                    sub_col_name='subject',
+                    obj_col_name='object')
+
+    tag_IBO_for_stc(IBO_data, True)
+    
+    IBO_data2 = load_IBO_task_data('./dataset/input/test_IBO.csv',
+                    stc_idx_col_name='sentence_idx',
+                    stc_col_name='stc',
+                    sub_col_name='subject',
+                    obj_col_name='object')
+
+    tag_IBO_POS_for_each_word(IBO_data2, True)
